dd31d5d022c8542744911a338bb01f122f798148
mcorbin <corbin.math@gmail.com>
2016-08-24 23:52:04 +0200

    add group module

--- src/modules/group.rs
+++ src/modules/group.rs
@@ -2,11 +2,15 @@ use std::fs;
 use std::num;
 use std::io::prelude::*;
 use std::io;
+use modules::test;
+use std::str;
+use util;
 
 #[derive(Debug)]
 pub enum GroupError {
     ParseInt(num::ParseIntError),
-    Io(io::Error)
+    Io(io::Error),
+    ParseBool(str::ParseBoolError)
 }
 
 
@@ -19,6 +23,12 @@ impl From<io::Error> for GroupError {
 impl From<num::ParseIntError> for GroupError {
     fn from(err: num::ParseIntError) -> GroupError {
         GroupError::ParseInt(err)
+    }
+}
+
+impl From<str::ParseBoolError> for GroupError {
+    fn from(err: str::ParseBoolError) -> GroupError {
+        GroupError::ParseBool(err)
     }
 }
 
@@ -55,6 +65,51 @@ pub fn parse_group_line(line: &Vec<String>) -> Result<Group, num::ParseIntError>
         password: password.to_string(),
         gid: gid
     })
+}
+
+fn check_gid(gid: i32, group: &Group, result: &mut test::TestResult) {
+    let test_name = "group - gid";
+    if group.gid == gid {
+        let success = test::UnitSuccess {
+            test: test_name.to_string(),
+            expected: format!("{}", gid),
+        };
+        result.success +=1;
+        result.summary.push(test::UnitResult::from(success))
+    } else {
+        let error = test::UnitError {
+            test: test_name.to_string(),
+            expected: format!("{}", gid),
+            actual: format!("{}", group.gid),
+            message: "incorrect gid".to_string(),
+        };
+        result.error +=1;
+        result.summary.push(test::UnitResult::from(error))
+    }
+}
+
+pub fn check(name: &str,
+             exists: &str,
+             gid: Option<&str>) -> Result<test::TestResult, GroupError> {
+    let mut result = test::TestResult {
+        success: 0,
+        error: 0,
+        summary: Vec::new()
+    };
+    let exists_bool: bool = try!(exists.parse());
+    let group_result = try!(get_group_from_name(name));
+    test::check_exists(&group_result, exists_bool, &mut result, "group - exists".to_string());
+    match group_result {
+        None => Ok(result),
+        Some(group) => {
+            let gid = try!(util::parse_int(gid));
+            gid.map(|gid_int| {
+                check_gid(gid_int, &group, &mut result);
+            });
+            Ok(result)
+        }
+
+    }
 }
 
 pub fn get_group_from_name(name: &str) -> Result<Option<Group>, GroupError> {
@@ -199,4 +254,95 @@ fn get_group_from_gid_test_success() {
     let group_opt = get_group_from_gid(99999).unwrap();
     assert!(group_opt.is_none())
 
+}
+
+#[test]
+fn check_gid_test_success() {
+    let mut result = test::TestResult {
+        success: 0,
+        error: 0,
+        summary: Vec::new()
+    };
+    let group = Group {
+        name: "group1".to_string(),
+        password: "x".to_string(),
+        gid: 2001
+    };
+    check_gid(2001, &group, &mut result);
+    assert_eq!(result.success, 1);
+    assert_eq!(result.error, 0);
+    assert_eq!(result.summary.len(), 1);
+    {
+        let ref summary = result.summary[0];
+        match summary {
+            &test::UnitResult::Success(ref s) => assert_eq!(s.expected, "2001"),
+            &test::UnitResult::Error(_) => panic!("Error in test")
+        }
+    }
+    check_gid(1, &group, &mut result);
+    assert_eq!(result.success, 1);
+    assert_eq!(result.error, 1);
+    assert_eq!(result.summary.len(), 2);
+    {
+        let ref summary = result.summary[1];
+        match summary {
+            &test::UnitResult::Success(_) => panic!("Error in test"),
+            &test::UnitResult::Error(ref s) => {
+                assert_eq!(s.expected, "1");
+                assert_eq!(s.actual, "2001")
+            }
+        }
+    }
+}
+
+
+#[test]
+fn check_test_success() {
+    let mut result = check("group1", "true", None).unwrap();
+    assert_eq!(result.error, 0);
+    assert_eq!(result.success, 1);
+    assert_eq!(result.summary.len(), 1);
+    {
+        let ref summary = result.summary[0];
+        match summary {
+            &test::UnitResult::Success(ref s) => assert_eq!(s.expected, "true"),
+            &test::UnitResult::Error(_) => panic!("Error in test")
+        }
+    }
+    result = check("group1", "true", Some("2001")).unwrap();
+    assert_eq!(result.error, 0);
+    assert_eq!(result.success, 2);
+    assert_eq!(result.summary.len(), 2);
+    {
+        let ref summary = result.summary[1];
+        match summary {
+            &test::UnitResult::Success(ref s) => assert_eq!(s.expected, "2001"),
+            &test::UnitResult::Error(_) => panic!("Error in test")
+        }
+    }
+    result = check("group1", "true", Some("2002")).unwrap();
+    assert_eq!(result.error, 1);
+    assert_eq!(result.success, 1);
+    assert_eq!(result.summary.len(), 2);
+    {
+        let ref summary = result.summary[1];
+        match summary {
+            &test::UnitResult::Success(_) => panic!("Error in test"),
+            &test::UnitResult::Error(ref err) => {
+                assert_eq!(err.expected, "2002");
+                assert_eq!(err.actual, "2001")
+            }
+        }
+    }
+    result = check("dontexists", "false", Some("2000")).unwrap();
+    assert_eq!(result.error, 0);
+    assert_eq!(result.success, 1);
+    assert_eq!(result.summary.len(), 1);
+    {
+        let ref summary = result.summary[0];
+        match summary {
+            &test::UnitResult::Success(ref s) => assert_eq!(s.expected, "false"),
+            &test::UnitResult::Error(_) => panic!("Error in test")
+        }
+    }
 }
