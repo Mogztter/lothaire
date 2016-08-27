7a409bc0a17323abce02125a30d5f1b05e429670
mcorbin <corbin.math@gmail.com>
2016-08-28 00:58:23 +0200

    Modify distrib info retrieve

--- src/distrib.rs
+++ src/distrib.rs
@@ -1,7 +1,15 @@
 use std::fs;
 
 fn is_rhel() -> bool {
-    let file = fs::File::open("/etc/redhat_release");
+    let file = fs::File::open("/etc/redhat-release");
+    match file {
+        Ok(_) => true,
+        Err(_) => false
+    }
+}
+
+fn is_centos() -> bool {
+    let file = fs::File::open("/etc/centos-release");
     match file {
         Ok(_) => true,
         Err(_) => false
@@ -17,11 +25,14 @@ fn is_debian() -> bool {
 }
 
 pub fn get_distrib() -> String {
-    if is_rhel() {
-        "rhel".to_string()
+    if is_centos() {
+        "centos".to_string()
     }
     else if is_debian() {
         "debian".to_string()
+    }
+    else if is_rhel() {
+        "rhel".to_string()
     }
     else {
         "unknow".to_string()
@@ -31,6 +42,7 @@ pub fn get_distrib() -> String {
 pub fn get_package_manager() -> String {
     match get_distrib().as_ref() {
         "rhel" => "rpm".to_string(),
+        "centos" => "rpm".to_string(),
         "debian" => "deb".to_string(),
         _ => "unknow".to_string()
     }
