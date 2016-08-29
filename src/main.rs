1008a73f34ee624a8f256fb54214a3835b11bab6
mcorbin <corbin.math@gmail.com>
2016-08-29 23:30:17 +0200

    add support for testing packages (rpm & deb)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ pub mod util;
 pub mod distrib;
 use modules::user;
 use modules::group;
+use modules::package;
+
 fn main() {
 
     let yaml = load_yaml!("cli.yaml");
@@ -45,6 +47,30 @@ fn main() {
         let exists = matches.value_of("exists").unwrap();
         let gid = matches.value_of("gid");
         let test_result = group::check(name, exists, gid);
+        match test_result {
+            Ok(result) => {
+                if result.error == 0 {
+                    println!("All tests success : {:?}", result);
+                    std::process::exit(0);
+                }
+                else {
+                    println!("Error during tests : {:?}", result);
+                    std::process::exit(1);
+                }
+            }
+            Err(error) => {
+                println!("System error : {:?}", error);
+                std::process::exit(2);
+            }
+        }
+    }
+
+    // package subcommand
+    if let Some(matches) = matches.subcommand_matches("package") {
+        let name = matches.value_of("name").unwrap();
+        let installed = matches.value_of("installed").unwrap();
+        let version = matches.value_of("version");
+        let test_result = package::check(name, installed, version);
         match test_result {
             Ok(result) => {
                 if result.error == 0 {
--- src/modules/package.rs
+++ src/modules/package.rs
@@ -2,8 +2,27 @@ use std::process::Command;
 use std::io;
 use distrib;
 use modules::test;
+use std::str;
 
-pub fn check_deb(package: &str, version: Option<&str>, installed: bool) -> Result<test::UnitResult, io::Error> {
+#[derive(Debug)]
+pub enum PackageError {
+    Io(io::Error),
+    ParseBool(str::ParseBoolError)
+}
+
+impl From<io::Error> for PackageError {
+    fn from(err: io::Error) -> PackageError {
+        PackageError::Io(err)
+    }
+}
+
+impl From<str::ParseBoolError> for PackageError {
+    fn from(err: str::ParseBoolError) -> PackageError {
+        PackageError::ParseBool(err)
+    }
+}
+
+pub fn check_deb(package: &str, version: Option<&str>, installed: bool, result: &mut test::TestResult) -> Result<(), io::Error> {
     let test_name = "package";
     let command_result = try!(Command::new("dpkg-query")
         .arg("-f")
@@ -43,7 +62,8 @@ pub fn check_deb(package: &str, version: Option<&str>, installed: bool) -> Resul
             test: test_name.to_string(),
             expected: expected_string
         };
-        Ok(test::UnitResult::from(success))
+        result.success += 1;
+        result.summary.push(test::UnitResult::from(success))
     }
     else {
         let error = test::UnitError {
@@ -52,11 +72,13 @@ pub fn check_deb(package: &str, version: Option<&str>, installed: bool) -> Resul
             actual: format!("package found: {}", found),
             message: "Package test fail".to_string(),
         };
-        Ok(test::UnitResult::from(error))
+        result.error += 1;
+        result.summary.push(test::UnitResult::from(error))
     }
+    Ok(())
 }
 
-pub fn check_rpm(package: &str, version: Option<&str>, installed: bool) -> Result<test::UnitResult, io::Error> {
+pub fn check_rpm(package: &str, version: Option<&str>, installed: bool, result: &mut test::TestResult) -> Result<(), io::Error> {
     let test_name = "package";
     let command_result = try!(Command::new("rpm")
         .arg("-q")
@@ -89,7 +111,8 @@ pub fn check_rpm(package: &str, version: Option<&str>, installed: bool) -> Resul
             test: test_name.to_string(),
             expected: expected_string
         };
-        Ok(test::UnitResult::from(success))
+        result.success += 1;
+        result.summary.push(test::UnitResult::from(success))
     }
     else {
         let error = test::UnitError {
@@ -98,10 +121,44 @@ pub fn check_rpm(package: &str, version: Option<&str>, installed: bool) -> Resul
             actual: format!("package found: {}", found),
             message: "Package test fail".to_string(),
         };
-        Ok(test::UnitResult::from(error))
+        result.error += 1;
+        result.summary.push(test::UnitResult::from(error))
     }
+    Ok(())
 }
 
+fn package_manager_not_found (package: &str, version: Option<&str>, installed: bool, result: &mut test::TestResult) {
+    let test_name = "package";
+    let expected_string = if version.is_some() {
+        format!("name: {}, version: {}, installed: {}", package, version.unwrap(), installed)
+    }
+    else {
+        format!("name: {}, installed: {}", package, installed)
+    };
+    let error = test::UnitError {
+        test: test_name.to_string(),
+        expected: expected_string,
+        actual: "package manager not found".to_string(),
+        message: "lothaire failed to determine your package manager".to_string(),
+    };
+    result.error += 1;
+    result.summary.push(test::UnitResult::from(error));
+}
+
+pub fn check(package: &str, installed: &str, version: Option<&str>) -> Result<test::TestResult, PackageError> {
+    let installed_bool: bool = try!(installed.parse());
+    let mut result = test::TestResult {
+        success: 0,
+        error: 0,
+        summary: Vec::new()
+    };
+    match distrib::get_package_manager().as_ref() {
+        "rpm" => try!(check_rpm(package, version, installed_bool, &mut result)),
+        "deb" => try!(check_deb(package, version, installed_bool, &mut result)),
+        _ => package_manager_not_found(package, version, installed_bool, &mut result)
+    };
+    Ok(result)
+}
 
 // TESTS
 
@@ -109,23 +166,28 @@ pub fn check_rpm(package: &str, version: Option<&str>, installed: bool) -> Resul
 #[test]
 fn check_rpm_test_success() {
     if distrib::get_package_manager() == "rpm" {
+        let mut result = test::TestResult {
+            success: 0,
+            error: 0,
+            summary: Vec::new()
+        };
         let openssl_version = "1.0.1e";
         let mut package_name = "openssl";
         // package exists and version ok
-        let  mut result = check_rpm(package_name, Some(openssl_version), true).unwrap();
-        match result {
+        let _ = check_rpm(package_name, Some(openssl_version), true, &mut result).unwrap();
+        match result.summary[0] {
             test::UnitResult::Success(ref s) => assert_eq!(s.expected, format!("name: {}, version: {}, installed: {}", package_name, openssl_version, true)),
             test::UnitResult::Error(_) => panic!("error in test")
         }
         // package exists and no version
-        result = check_rpm(package_name, None, true).unwrap();
-        match result {
+        let _ = check_rpm(package_name, None, true, &mut result).unwrap();
+        match result.summary[1] {
             test::UnitResult::Success(ref s) => assert_eq!(s.expected, format!("name: {}, installed: {}", package_name, true)),
             test::UnitResult::Error(_) => panic!("error in test")
         }
         // package exists but incorrect version
-        result = check_rpm(package_name, Some("1.0.2"), true).unwrap();
-        match result {
+        let _ = check_rpm(package_name, Some("1.0.2"), true, &mut result).unwrap();
+        match result.summary[2] {
             test::UnitResult::Error(ref s) => {
                 assert_eq!(s.expected, format!("name: {}, version: {}, installed: {}", package_name, "1.0.2", true));
                 assert_eq!(s.actual, "package found: false");
@@ -134,14 +196,14 @@ fn check_rpm_test_success() {
         }
         // package dont exists and no version and test not installed
         package_name = "notexists";
-        result = check_rpm(package_name, None, false).unwrap();
-        match result {
+        let _ = check_rpm(package_name, None, false, &mut result).unwrap();
+        match result.summary[3] {
             test::UnitResult::Success(ref s) => assert_eq!(s.expected, format!("name: {}, installed: {}", package_name, false)),
             test::UnitResult::Error(_) => panic!("error in test")
         }
         // package dont exists and no version and test installed
-        result = check_rpm(package_name, None, true).unwrap();
-        match result {
+        let _ = check_rpm(package_name, None, true, &mut result).unwrap();
+        match result.summary[4] {
             test::UnitResult::Error(ref e) => {
                 assert_eq!(e.expected, format!("name: {}, installed: {}", package_name, true));
                 assert_eq!(e.actual, format!("package found: {}", false));
@@ -149,8 +211,8 @@ fn check_rpm_test_success() {
             test::UnitResult::Success(_) => panic!("error in test")
         }
         // package not exists and incorrect version
-        result = check_rpm(package_name, Some("1"), true).unwrap();
-        match result {
+        let _ = check_rpm(package_name, Some("1"), true, &mut result).unwrap();
+        match result.summary[5] {
             test::UnitResult::Error(ref e) => {
                 assert_eq!(e.expected, format!("name: {}, version: {}, installed: {}", package_name, "1", true));
                 assert_eq!(e.actual, format!("package found: {}", false));
@@ -163,23 +225,28 @@ fn check_rpm_test_success() {
 #[test]
 fn check_deb_test_success() {
     if distrib::get_package_manager() == "deb" {
+        let mut result = test::TestResult {
+            success: 0,
+            error: 0,
+            summary: Vec::new()
+        };
         let openssl_version = "1.0.1t-1+deb8u2";
         let mut package_name = "openssl";
         // package exists and version ok
-        let mut result = check_deb(package_name, Some(openssl_version), true).unwrap();
-        match result {
+        let _ = check_deb(package_name, Some(openssl_version), true, &mut result);
+        match result.summary[0] {
             test::UnitResult::Success(ref s) => assert_eq!(s.expected, format!("name: {}, version: {}, installed: {}", package_name, openssl_version, true)),
             test::UnitResult::Error(_) => panic!("error in test")
         }
         // package exists and no version
-        result = check_deb(package_name, None, true).unwrap();
-        match result {
+        let _ = check_deb(package_name, None, true, &mut result);
+        match result.summary[1] {
             test::UnitResult::Success(ref s) => assert_eq!(s.expected, format!("name: {}, installed: {}", package_name, true)),
             test::UnitResult::Error(_) => panic!("error in test")
         }
         // package exists but incorrect version
-        result = check_deb(package_name, Some("1.0.2"), true).unwrap();
-        match result {
+        let _ = check_deb(package_name, Some("1.0.2"), true, &mut result);
+        match result.summary[2] {
             test::UnitResult::Error(ref s) => {
                 assert_eq!(s.expected, format!("name: {}, version: {}, installed: {}", package_name, "1.0.2", true));
                 assert_eq!(s.actual, "package found: false");
@@ -188,14 +255,14 @@ fn check_deb_test_success() {
         }
         // package dont exists and no version and test not installed
         package_name = "notexists";
-        result = check_deb(package_name, None, false).unwrap();
-        match result {
+        let _ = check_deb(package_name, None, false, &mut result);
+        match result.summary[3] {
             test::UnitResult::Success(ref s) => assert_eq!(s.expected, format!("name: {}, installed: {}", package_name, false)),
             test::UnitResult::Error(_) => panic!("error in test")
         }
         // package dont exists and no version and test installed
-        result = check_deb(package_name, None, true).unwrap();
-        match result {
+        let _ = check_deb(package_name, None, true, &mut result);
+        match result.summary[4] {
             test::UnitResult::Error(ref e) => {
                 assert_eq!(e.expected, format!("name: {}, installed: {}", package_name, true));
                 assert_eq!(e.actual, format!("package found: {}", false));
@@ -203,8 +270,8 @@ fn check_deb_test_success() {
             test::UnitResult::Success(_) => panic!("error in test")
         }
         // package not exists and incorrect version
-        result = check_deb(package_name, Some("1"), true).unwrap();
-        match result {
+        let _ = check_deb(package_name, Some("1"), true, &mut result);
+        match result.summary[5] {
             test::UnitResult::Error(ref e) => {
                 assert_eq!(e.expected, format!("name: {}, version: {}, installed: {}", package_name, "1", true));
                 assert_eq!(e.actual, format!("package found: {}", false));
